e493d0b8e7965caac90ac043984ea0eb6b69b701
Sean Lerner <sean@@smallcity.ca>
2018-03-10 17:11:04 -0500

    more robust for different scenarios

--- main.js
+++ main.js
@@ -1,11 +1,38 @@
 module.exports = class {
 
   constructor() {
-    this.trigger = 'hw'
+    this.trigger   = 'hw'
+    this.nice_name = 'Hello World Trigger'
   }
 
-  run(resolve, _reject) {
-    resolve('Hello, World!')
+  run(run) {
+    setTimeout(() => {
+
+      let result = 'Hello, '
+
+      if (run.args)
+        result += run.args
+      else
+        result += 'World'
+
+      result += '!'
+
+      if (run.args == 'object')
+        run.resolve({ title: 'Hello', subtitle: 'World', body: "Here's your object."})
+      else if (run.args == 'undefined')
+        run.resolve()
+      else if (run.args == 'undefined-fail')
+        run.reject()
+      else if (run.args == 'fail')
+        run.reject('Goodbye, World!')
+      else if (run.args == 'crash')
+        beep
+      else if (run.args)
+        run.resolve(`Hello, ${run.args}!`)
+      else
+        run.resolve('Hello, World!')
+
+    }, 2500)
   }
 
 }
